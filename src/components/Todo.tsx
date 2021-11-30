49cda72d266cfe738d10e239371e4cd21f947808
HDYS-TTBYS <tatehide151@outlook.jp>
2021-11-30 22:03:39 +0900

    3-19

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import { VFC, useState } from 'react';
+import { LogoutIcon } from '@heroicons/react/outline';
+import { ShieldCheckIcon } from '@heroicons/react/solid';
+import { useAppSelector, useAppDispatch } from '../app/hooks';
+import { setEditedTask, selectTask } from '../slices/appSlice';
+import { useProcessAuth } from '../hooks/useProcessAuth';
 
-export const Todo = () => {
-  return <div></div>;
+export const Todo: VFC = () => {
+  const { logout } = useProcessAuth();
+
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
+      <LogoutIcon
+        onClick={logout}
+        className="h-7 w-7 mt-1 mb-5 text-blue-500 cursor-pointer"
+      />
+    </div>
+  );
 };
